d9750bbbf2f36e9ea8ebb78d3fdedf991f8fa8c9
Kishanmc <1by23is095@bmsit.in>
2025-03-26 17:56:56 +0530

    krpyton

---
+++ client/src/pages/annimation3d.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Annimation3D = () => {
+  return (
+    <div>annimation3d</div>
+  )
+}
+
+export default Annimation3D
---
+++ client/src/pages/lab3d.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lab3D = () => {
+  return (
+    <div>Lab3D</div>
+  )
+}
+
+export default Lab3D
