9f7e3af0a6efda7e86b09c518decd398fb49b01b
Kishan M C <1by23is095@bmsit.in>
2025-12-15 11:00:42 +0530

    Delete backend directory

--- backend/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-exports.authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ msg: `User role '${req.user.role}' is not authorized` });
-    }
-    next();
-  };
-};
